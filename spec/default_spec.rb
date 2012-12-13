3594eeb32a206bee819256cead04bcd7e5ec4628
Chris Lundquist <chris.lundquist@bluebox.net>
2012-12-12 18:12:40 -0800

    implement default spec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,8 +1,11 @@
 require 'chefspec'
 
-describe 'openvz/::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'openvz/::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+describe 'openvz::default' do
+  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'openvz::default' }
+  it 'should include each recipe' do
+    chef_run.should include_recipe 'openvz::repo'
+    chef_run.should include_recipe 'openvz::install'
+    chef_run.should include_recipe 'openvz::services'
+    chef_run.should include_recipe 'openvz::sysctl'
   end
 end
