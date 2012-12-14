7079e7cd68e514f0e0833f08064f3a7a52e6907f
Chris Lundquist <chris.lundquist@bluebox.net>
2012-12-13 18:54:49 -0800

    [repo] only install gpg_key if present

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,1 +1,2 @@
 default['openvz']['yum']['repo_url'] = 'http://download.openvz.org/openvz.repo'
+default['openvz']['yum']['gpg_key'] = 'http://download.openvz.org/RPM-GPG-Key-OpenVZ'
--- recipes/repo.rb
+++ recipes/repo.rb
@@ -3,8 +3,10 @@ remote_file "/etc/yum.repos.d/openvz.repo" do
   mode 00644
   owner "root"
   group "root"
+  not_if { File.exists? "/etc/yum.repos.d/openvz.repo" }
 end
 
 bash "Import Openvz GPG key" do
-  code "rpm --import  http://download.openvz.org/RPM-GPG-Key-OpenVZ"
+  code "rpm --import #{node.openvz.yum.gpg_key}"
+  only_if { node.openvz.yum.gpg_key }
 end
